d8f82d0d84006b3dabb15b10d553768a4c90d7d3
nicusor43 <53815814+nicusor43@users.noreply.github.com>
2020-05-13 23:58:20 +0300

    add some commands i guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import asyncio
 
 import discord
 from dotenv import load_dotenv
@@ -13,43 +14,63 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 
-@client.event
-async def on_ready():
-    print(f'{client.user.name} s-a conectat la server')
-
-
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Sal {member.name}, bine ai venit in server-ul meu'
-    )
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user or message.content[0] == '!':
-        return
-
-    await message.channel.send("esti smecher @nicusor43")
-
-
 @bot.command(name="boss")
 async def help_command(ctx):
     await ctx.send("Acest bot este facut de nicusor43 etc etc")
 
+
 @bot.command(name="zar")
 async def zar_command(ctx):
     if ctx.author == "nicusor43":
-        numar = random.choice(range(3,7))
+        numar = random.choice(range(3, 7))
     else:
-        numar = random.choice(range(1,7))
+        numar = random.choice(range(1, 7))
     await ctx.send(f'Ai dat {numar}')
+
 
 @bot.command(name="greet")
 async def greet_command(ctx):
     await ctx.send(f'Bine ai venit, {ctx.author}')
 
+
+@bot.command(name="laba", description="Afla cine-i labagiu")
+async def swear_command(ctx):
+    if "nicusor43" in str(ctx.author):
+        await ctx.send(f'Acest utilizator nu da laba')
+    else:
+        await ctx.send(f'{ctx.author} e un mare labagiu')
+
+
+@bot.command(name="guess", description="Ghiceste un numar intre 1 si 100")
+async def guess_command(context):
+    number = random.randint(1,101)
+    for guess in range(0, 5):
+        await context.send('Pick a number between 1 and 100')
+        msg = await client.wait_for('message', check=check)
+        attempt = int(msg.content)
+        if attempt > number:
+            await context.send(str(guess) + ' guesses left...')
+            await asyncio.sleep(1)
+            await context.send('Try going lower')
+            await asyncio.sleep(1)
+        elif attempt < number:
+            await context.send(str(guess) + ' guesses left...')
+            await asyncio.sleep(1)
+            await context.send('Try going higher')
+            await asyncio.sleep(1)
+
+        else:
+            await context.send('You guessed it! Good job!')
+            break
+    else:
+        await context.send("You didn't get it")
+
+def check(message):
+    try:
+        int(message.content)
+        return True
+    except ValueError:
+        return False
 
 bot.run(TOKEN)
 client.run(TOKEN)
